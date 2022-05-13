d130fd2be1b0b0b47ac20245f334451a0a03ef7b
felipehenriquee <fh27.95@gmail.com>
2022-05-13 14:55:13 -0300

    troca bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,11 @@ import { ArmodelsModule } from './armodels/armodels.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb://localhost/simplexr'), ClicksModule, SitesModule, ArmodelsModule],
+  imports: [ 
+    MongooseModule.forRoot('mongodb+srv://felipeTable:dviYXi7lDLMro5L6@cluster0.yik6j.mongodb.net/simplexr?retryWrites=true&w=majority'),
+    // MongooseModule.forRoot('mongodb+srv://FelipeHenrique:DjCXPhQcLEdXkW68@cluster0.mav8k.mongodb.net/Deployments?retryWrites=true&w=majority'), 
+    // MongooseModule.forRoot('mongodb://localhost/simplexr'), 
+    ClicksModule, SitesModule, ArmodelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
